34a63f2047fc461ea134e8390f7b87c57139f299
PEIYUN CHU <pchu2018@gmail.com>
2023-02-08 21:36:29 -0500

    added gifs, ready for launch

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -6,28 +6,30 @@ export default function MainCard() {
       <div className='maincard' id='card1'>
         <div className='maincard-text'>
           <h2>Automatic route parsing</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p>Set a server folder path or use the default, and Errordactyl will read your files and find all declared endpoints.</p>
         </div> 
         <ManageSearch></ManageSearch>
       </div>
+      <img className='gif' src='https://errordactyl.s3.amazonaws.com/ezgif-3-b69af5ed38.gif'></img>
       <div className='maincard' id='card2'>
         <div className='maincard-text'>
           <h2>Highly configurable</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p>Input request bodies, set URL parameters, add headers. Choose which routes to test or just run them all at once!</p>
         </div> 
         <CheckCircleOutlined></CheckCircleOutlined>
       </div>
       <div className='maincard' id='card3'>
         <div className='maincard-text'>
           <h2>Send batch requests</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p>No more manually sending one request at a time. Test all your endpoints together and quickly pinpoint route issues.</p>
         </div> 
         <SyncAlt></SyncAlt>
       </div>
+      <img className='gif' src='https://errordactyl.s3.amazonaws.com/ezgif-3-6a8eba0a17.gif'></img>
       <div className='maincard' id='card4'>
         <div className='maincard-text'>
           <h2>Robust error tracing</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <p>Errordactyl will parse your stack trace and return relevant information, saving you time and energy as you debug servers.</p>
         </div> 
         <ReportGmailerrorredOutlined></ReportGmailerrorredOutlined>
       </div>
